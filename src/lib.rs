9f4d8883db52287d3ee5dfcb2d6741320d2da57d
Amine RACHYD <amine.rachyd@amadeus.com>
2023-06-12 16:16:42 +0200

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -3,35 +3,100 @@ use std::{
     fs::{self, File},
     io::{Error, ErrorKind, Read, Write},
     os::unix::process::CommandExt,
-    process::{self},
+    process,
 };
 
 use chrono::Datelike;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct AppConfig {
+pub struct ConfigFile {
     pub editor: String,
     pub drafts_path: String,
     pub file_extension: String,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct AppConfig {
+    pub config_file_path: String,
+    pub config_file: ConfigFile,
+}
+
+// Runs the program
+pub fn run(app_config: &AppConfig) {
+    check_draft_path(&app_config);
+
+    let latest_file_path = latest_file_path(&app_config);
+
+    if check_file_exists(&latest_file_path) == true {
+        open_file_with_editor(&latest_file_path, &app_config).unwrap();
+    } else {
+        create_and_open_file_with_editor(&latest_file_path, &app_config).unwrap();
+    }
+}
+
+// Runs subcommand
+pub fn run_subcommand(command: &str, app_config: &AppConfig) {
+    if command.eq("config") {
+        open_config_file(&app_config);
+    } else if command.eq("commit") {
+        commit_drafts(&app_config);
+    } else {
+        println!("Unknown command, exiting...");
+        process::exit(1)
+    }
+}
+
+/**
+ * Subcommand: Opens config file for editing
+ */
+fn open_config_file(app_config: &AppConfig) {
+    println!("Opening config file...");
+
+    if open_file_with_editor(&app_config.config_file_path, app_config).is_err() {
+        println!("Config file doesn't exist, creating one...");
+        create_and_open_file_with_editor(&app_config.config_file_path, app_config).unwrap();
+    }
+}
+
+/**
+ * Subcommand: Commits drafts
+ * TODO
+ */
+fn commit_drafts(app_config: &AppConfig) {
+    println!("Commiting draft files...");
+
+    process::Command::new("cd")
+        .arg(&app_config.config_file.drafts_path)
+        .exec();
+    process::Command::new("git").args(["add", "."]).exec();
+    process::Command::new("git")
+        .args(["commit", "-m", "Test commit"])
+        .exec();
+    unimplemented!()
+}
+
 impl AppConfig {
-    fn default_config() -> Self {
-        let HOME = env!("HOME");
-        let config_file_path = format!("{}/.config/drafting.yaml", HOME);
-
-        let app_config = AppConfig {
-            editor: "vim".to_owned(),
-            drafts_path: format!("{}/drafts", HOME),
-            file_extension: "md".to_owned(),
-        };
-
-        let yaml = serde_yaml::to_string(&app_config).unwrap();
+    fn create_default_config() -> Self {
+        let home = env!("HOME");
+        let config_file_path = format!("{}/.config/drafting.yaml", home);
 
         println!("Writing default config to {}", config_file_path);
 
-        let mut f = fs::File::create(config_file_path).unwrap();
+        let config_file = ConfigFile {
+            editor: "vim".to_owned(),
+            drafts_path: format!("{}/drafts", home),
+            file_extension: "md".to_owned(),
+        };
+
+        let yaml = serde_yaml::to_string(&config_file).unwrap();
+
+        let app_config = AppConfig {
+            config_file_path,
+            config_file,
+        };
+
+        let mut f = fs::File::create(&app_config.config_file_path).unwrap();
 
         let result = f.write_all(yaml.as_bytes());
 
@@ -52,22 +117,31 @@ impl AppConfig {
         app_config
     }
 
-    fn parse_config(mut f: File) -> Self {
+    fn parse_config(mut f: File, config_file_path: String) -> Self {
         let mut buffer = "".to_owned();
 
         let editor = std::env::var("EDITOR");
 
         f.read_to_string(&mut buffer).unwrap();
 
-        let result = serde_yaml::from_str(&buffer);
+        let result = serde_yaml::from_str::<ConfigFile>(&buffer);
         match result {
             Err(_) => {
                 println!("Failed to parse config file, probably wrong format, exiting...");
                 process::exit(1)
             }
-            Ok(config) => match editor {
-                Ok(editor) => AppConfig { editor, ..config },
-                Err(_) => config,
+            Ok(config_file) => match editor {
+                Ok(editor) => AppConfig {
+                    config_file_path,
+                    config_file: ConfigFile {
+                        editor,
+                        ..config_file
+                    },
+                },
+                Err(_) => AppConfig {
+                    config_file_path,
+                    config_file,
+                },
             },
         }
     }
@@ -75,16 +149,16 @@ impl AppConfig {
 
 // Reads config file, used by application during startup
 pub fn read_app_config() -> AppConfig {
-    let HOME = env!("HOME");
-    let config_file_path = format!("{}/.config/drafting.yaml", HOME);
+    let home = env!("HOME");
+    let config_file_path = format!("{}/.config/drafting.yaml", home);
 
-    let config_file = fs::File::open(config_file_path);
+    let config_file = fs::File::open(config_file_path.clone());
 
     match config_file {
-        Ok(f) => AppConfig::parse_config(f),
+        Ok(f) => AppConfig::parse_config(f, config_file_path),
 
         Err(e) => match e.kind() {
-            ErrorKind::NotFound => AppConfig::default_config(),
+            ErrorKind::NotFound => AppConfig::create_default_config(),
             _ => {
                 println!("Error reading config file, exiting...");
                 process::exit(1)
@@ -94,7 +168,7 @@ pub fn read_app_config() -> AppConfig {
 }
 
 // Check file existance
-pub fn check_file_exists(path: &str) -> bool {
+fn check_file_exists(path: &str) -> bool {
     match fs::File::open(path) {
         Ok(_) => true,
         Err(_) => false,
@@ -102,14 +176,15 @@ pub fn check_file_exists(path: &str) -> bool {
 }
 
 // Check drafts directory and create it if it doesn't exist
-pub fn check_draft_path(path: &str) {
-    let result = fs::read_dir(path);
+fn check_draft_path(app_config: &AppConfig) {
+    let drafts_path = &app_config.config_file.drafts_path;
+    let result = fs::read_dir(drafts_path);
     match result {
         Ok(_) => {}
         Err(e) => match e.kind() {
             ErrorKind::NotFound => {
                 println!("Drafts directory doesn't exist, creating it...");
-                fs::create_dir(path).unwrap();
+                fs::create_dir(drafts_path).unwrap();
             }
             _ => {
                 println!("Error reading drafts directory, exiting...");
@@ -119,8 +194,33 @@ pub fn check_draft_path(path: &str) {
     }
 }
 
+// Get path of latest file type
+fn latest_file_path(app_config: &AppConfig) -> String {
+    format!(
+        "{}/{}.{}",
+        app_config.config_file.drafts_path,
+        latest_timestamp(),
+        app_config.config_file.file_extension
+    )
+}
+
+fn open_file_with_editor(file_path: &str, app_config: &AppConfig) -> Result<(), Error> {
+    process::Command::new(&app_config.config_file.editor)
+        .arg(&file_path)
+        .exec();
+
+    Ok(())
+}
+
+// Creates and opens file with editor in question
+fn create_and_open_file_with_editor(file_path: &str, app_config: &AppConfig) -> Result<(), Error> {
+    fs::File::create(file_path)?;
+
+    open_file_with_editor(file_path, app_config)
+}
+
 // Get latest timestamp
-pub fn latest_timestamp() -> String {
+fn latest_timestamp() -> String {
     let current_date = chrono::Utc::now();
 
     let day = current_date.day();
@@ -128,35 +228,4 @@ pub fn latest_timestamp() -> String {
     let year = current_date.year();
 
     format!("{}_{}_{}_draft", year, month, day)
-}
-
-pub fn open_file_with_editor(file_path: &str, editor: &str) -> Result<(), Error> {
-    process::Command::new(editor).arg(&file_path).exec();
-
-    Ok(())
-}
-
-// Creates and opens file with editor in question
-pub fn create_and_open_file_with_editor(file_path: &str, editor: &str) -> Result<(), Error> {
-    fs::File::create(file_path)?;
-
-    open_file_with_editor(file_path, editor)
-}
-
-// Runs COMMAND: "drafting COMMAND"
-pub fn run_subcommand(command: &str) {
-    if command.eq("config") {
-        println!("Opening config file...");
-
-        let HOME = env!("HOME");
-        let config_file_path = format!("{}/.config/drafting.yaml", HOME);
-
-        if open_file_with_editor(&config_file_path, "vim").is_err() {
-            println!("Config file doesn't exist, creating one...");
-            create_and_open_file_with_editor(&config_file_path, "vim");
-        }
-    } else {
-        println!("Unknown command, exiting...");
-        process::exit(1)
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,14 @@ use drafting::*;
 fn main() {
     let mut args = args();
     let subcommand = args.nth(1);
+    let app_config = read_app_config();
+
     match subcommand {
         Some(command) => {
-            run_subcommand(&command);
+            run_subcommand(&command, &app_config);
         }
         None => {
-            let app_config = read_app_config();
-
-            let AppConfig {
-                editor,
-                drafts_path,
-                file_extension,
-            } = app_config;
-
-            check_draft_path(&drafts_path);
-
-            let file_path = format!("{}/{}.{}", drafts_path, latest_timestamp(), file_extension);
-
-            if check_file_exists(&file_path) == true {
-                open_file_with_editor(&file_path, &editor).unwrap();
-            } else {
-                create_and_open_file_with_editor(&file_path, &editor).unwrap();
-            }
+            run(&app_config);
         }
     }
 }
